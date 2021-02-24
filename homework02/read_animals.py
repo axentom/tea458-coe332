9b5dfadf0ac4115380ffa0c1c43d3c1f56768ede
Thomas Axen <axentom@utexas.edu>
2021-02-24 11:47:12 -0600

    Added in functions to check that the input indices are appropriate and to check that the animals.json file is of an appropriate size in order to breed animals

--- homework02/read_animals.py
+++ homework02/read_animals.py
@@ -1,20 +1,37 @@
 import json
 import random
+import sys
 
-def main():
+def check_animals_dict_length(): # Check to see if animals.json has enough values in it to breed
+    with open('animals.json', 'r') as f:
+        animals = json.load(f)
+    count = len(animals['animal'])
+    if (count < 2):
+        return('animals dict length FAIL')
+        sys.exit()
+    else:
+        return('animals dict length pass')
+
+def check_parents_index_range(parent1_index, parent2_index): # Checks that the input indices are within the range of animals.json
+    with open('animals.json', 'r') as f:
+        animals = json.load(f)
+    count = len(animals['animal'])
+    if (parent1_index not in range(0,count)):
+        return(f'Parent 1 index: {parent1_index} range FAIL')
+        sys.exit()
+    elif (parent2_index not in range(0,count)):
+        return(f'Parent 2 index: {parent2_index} range FAIL')
+        sys.exit()
+    else:
+        return(f'Parent indices: {parent1_index} and {parent2_index} range pass')
+
+def create_children(parent1_index, parent2_index):
     with open('animals.json', 'r') as f:
         animals = json.load(f)
 
-    print('One random animal:')
-    print(animals['animal'][random.randint(0,19)])
-
-# Construct new dict for parents
-    parent1_index = random.randint(0,19)
-    parent2_index = random.randint(0,19)
-    while (parent2_index == parent1_index):
-        parent2_index = random.randint(0,19)
+# Grab parents from input indices
     parents_list = {}
-    parents_list['animal'] = [] 
+    parents_list['animal'] = []
     parents_list['animal'].append(animals['animal'][parent1_index])
     parents_list['animal'].append(animals['animal'][parent2_index])
 
@@ -43,6 +60,22 @@ def main():
     print(parents_list['animal'][1])
     print('\nChild of the two random parents:')
     print(child_list['animal'][0])
+
+def random_animal():
+    with open('animals.json', 'r') as f:
+        animals = json.load(f)
+
+    print('One random animal:')
+    print(animals['animal'][random.randint(0,19)])
+
+def main():
+    p1 = int(sys.argv[1])
+    p2 = int(sys.argv[2])
+    
+    print(check_animals_dict_length())
+    print(check_parents_index_range(p1,p2))
+    random_animal()
+    create_children(p1,p2)
 
 if __name__ == "__main__":
     main()
